1bbf55478842363d8459a2fc125a6e2b3307348b
Aflaungos <vimavick@gmail.com>
2026-08-07 18:32:01 -0300

    fix
    Change-Id: I74e0faa95c251c1e76f4b7fa1e5109b5a740de2d

--- omni_evert.mk
+++ omni_evert.mk
@@ -14,8 +14,7 @@
 # limitations under the License.
 #
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
